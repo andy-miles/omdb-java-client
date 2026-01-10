91f6621f7c6c4e4fbf5f4688a40e089ab9539bfd
Andrew Miles <35545964+andy-miles@users.noreply.github.com>
2026-01-10 13:36:39 -0800

    Updated copyright

--- src/main/java/com/amilesend/omdb/client/model/type/MediaType.java
+++ src/main/java/com/amilesend/omdb/client/model/type/MediaType.java
@@ -1,6 +1,6 @@
 /*
  * omdb-java-client - A client to access the OMDb API
- * Copyright © 2025 Andy Miles (andy.miles@amilesend.com)
+ * Copyright © 2025-2026 Andy Miles (andy.miles@amilesend.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
